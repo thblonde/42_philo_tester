6f7e462470ce7522666ae4ece8d6fd7630f12685
thblonde <blondet.thomas@protonmail.com>
2025-01-28 16:26:13 +0100

    Fix syntax

--- tester.sh
+++ tester.sh
@@ -4,7 +4,8 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 
 echo "Test 1 800 200 200 : The philosopher should not eat and should die.";echo
-if ./philo 1 800 200 200 | grep dead >/dev/null ;then
+if ./philo 1 800 200 200 | grep dead >/dev/null
+then
     echo -e "${GREEN}OK"
 else
     echo -e "${RED}KO"
@@ -13,7 +14,8 @@ fi
 echo -e "\033[0m"
 
 echo; echo "Test 5 800 200 200 : No philosopher should die. (10s test)";echo
-if timeout 10 ./philo 5 800 200 200 | grep dead >/dev/null ;then
+if timeout 10 ./philo 5 800 200 200 | grep dead >/dev/null
+then
     echo -e "${RED}KO"
 else
     echo -e "${GREEN}OK"
@@ -22,7 +24,8 @@ fi
 echo -e "\033[0m"
 
 echo; echo "Test 5 800 200 200 : No philosopher should die and the simulation should stop when every philosopher has eaten at least 7 times.";echo
-if ./philo 5 800 200 200 7 | grep dead >/dev/null ;then
+if ./philo 5 800 200 200 7 | grep dead >/dev/null
+then
     echo -e "${RED}KO"
 else
     echo -e "${GREEN}OK"
@@ -31,7 +34,8 @@ fi
 echo -e "\033[0m"
 
 echo; echo "Test 4 410 200 200 : No philosopher should die. (10s test)";echo
-if timeout 10 ./philo 4 410 200 200 | grep dead >/dev/null ;then
+if timeout 10 ./philo 4 410 200 200 | grep dead >/dev/null
+then
     echo -e "${RED}KO"
 else
     echo -e "${GREEN}OK"
@@ -40,7 +44,8 @@ fi
 echo -e "\033[0m"
 
 echo; echo "Test 3 310 200 100 : One philosopher should die.";echo
-if ./philo 4 310 200 100 | grep dead >/dev/null ;then
+if ./philo 4 310 200 100 | grep dead >/dev/null
+then
     echo -e "${GREEN}OK"
 else
     echo -e "${RED}KO"
@@ -49,7 +54,8 @@ fi
 echo -e "\033[0m"
 
 echo; echo "Test 2 400 200 200 : No philosopher should die. (10s test)";echo
-if timeout 10 ./philo 2 400 200 200 | grep dead >/dev/null ;then
+if timeout 10 ./philo 2 400 200 200 | grep dead >/dev/null
+then
     echo -e "${RED}KO"
 else
     echo -e "${GREEN}OK"
