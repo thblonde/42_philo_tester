3399e1943206cb1a45154ad2fe62e1afe18a9e08
thblonde <blondet.thomas@protonmail.com>
2025-02-02 16:23:29 +0100

    Fix argument check

--- tester.sh
+++ tester.sh
@@ -1,197 +1,208 @@
-cd ..; make >/dev/null
+cd ..; make >/dev/null; echo
+rm -f trace
 
-./philo 2>tmp
-echo "Number of argument";echo
-output=$(wc -l <tmp)
-if [ $output -eq 1 ]
+# Check argument validity
+./philo >output 2>&1
+echo -n "Argument check: "
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
 then
     printf "\033[0;32mOK\033[0m"
-    ./philo 1 2>tmp
-    output=$(wc -l <tmp)
-    if [ $output -eq 1 ]
-    then
-        printf " \033[0;32mOK\033[0m"
-        ./philo 1 2 2>tmp
-        output=$(wc -l <tmp)
-        if [ $output -eq 1 ]
-        then
-            printf " \033[0;32mOK\033[0m"
-            ./philo 1 2 3 2>tmp
-            output=$(wc -l <tmp)
-            if [ $output -eq 1 ]
-            then
-                printf " \033[0;32mOK\033[0m"
-                ./philo 1 2 3 4 2>tmp
-                output=$(wc -l <tmp)
-                if [ $output -eq 1 ]
-                then
-                    printf " \033[0;32mOK\033[0m"
-                    ./philo 1 2 3 4 5 6 7 2>tmp
-                    output=$(wc -l <tmp)
-                    if [ $output -eq 1 ]
-                    then
-                        printf " \033[0;32mOK\033[0m\n"
-                    else
-                        printf "\033[0;31mKO\033[0m\n"
-                    fi
-                else
-                    printf "\033[0;31mKO\033[0m\n"
-                fi
-            else
-                printf "\033[0;31mKO\033[0m\n"
-            fi
-        else
-            printf "\033[0;31mKO\033[0m\n"
-        fi
-    else
-        printf "\033[0;31mKO\033[0m\n"
-    fi
 else
-    printf "\033[0;31mKO\033[0m\n"
+    printf "\033[0;31mKO\033[0m"
+    echo "./philo: \033[0;31mKO\033[0m" >trace
 fi
-echo
 
-
-./philo -5 800 200 200 2>tmp
-echo "Argument validity";echo
-output=$(wc -l <tmp)
-if [ $output -eq 1 ]
+./philo 1 >output 2>&1
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
 then
-    printf "\033[0;32mOK\033[0m"
-    ./philo 5 -800 200 200 2>tmp
-    output=$(wc -l <tmp)
-    if [ $output -eq 1 ]
-    then
-        printf " \033[0;32mOK\033[0m"
-        ./philo 5 800 -200 200 2>tmp
-        output=$(wc -l <tmp)
-        if [ $output -eq 1 ]
-        then
-            printf " \033[0;32mOK\033[0m"
-            ./philo 5 800 200 -200 2>tmp
-            output=$(wc -l <tmp)
-            if [ $output -eq 1 ]
-            then
-                printf " \033[0;32mOK\033[0m"
-                ./philo 5 800 200 200 -7 2>tmp
-                output=$(wc -l <tmp)
-                if [ $output -eq 1 ]
-                then
-                    printf " \033[0;32mOK\033[0m\n"
-                else
-                    printf "\033[0;31mKO\033[0m\n"
-                fi
-            else
-                printf "\033[0;31mKO\033[0m\n"
-            fi
-        else
-            printf "\033[0;31mKO\033[0m\n"
-        fi
-    else
-        printf "\033[0;31mKO\033[0m\n"
-    fi
+    printf " \033[0;32mOK\033[0m"
 else
-    printf "\033[0;31mKO\033[0m\n"
+    printf " \033[0;31mKO\033[0m"
+    echo "./philo 1: \033[0;31mKO\033[0m" >>trace
 fi
-echo
+
+./philo 1 2 >output 2>&1
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
+then
+    printf " \033[0;32mOK\033[0m"
+else
+    printf " \033[0;31mKO\033[0m"
+    echo "./philo 1 2: \033[0;31mKO\033[0m" >>trace
+fi
+
+./philo 1 2 3 >output 2>&1
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
+then
+    printf " \033[0;32mOK\033[0m"
+else
+    printf " \033[0;31mKO\033[0m"
+    echo "./philo 1 2 3: \033[0;31mKO\033[0m" >>trace
+fi
+
+./philo 1 2 3 4 >output 2>&1
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
+then
+    printf " \033[0;32mOK\033[0m"
+else
+    printf " \033[0;31mKO\033[0m"
+    echo "./philo 1 2 3 4: \033[0;31mKO\033[0m" >>trace
+fi
+
+./philo 1 2 3 4 5 6 7 >output 2>&1
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
+then
+    printf " \033[0;32mOK\033[0m"
+else
+    printf " \033[0;31mKO\033[0m"
+    echo "./philo 1 2 3 4 5 6 7: \033[0;31mKO\033[0m" >>trace
+fi
+
+./philo -5 800 200 200 >output 2>&1
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
+then
+    printf " \033[0;32mOK\033[0m"
+else
+    printf " \033[0;31mKO\033[0m"
+    echo "./philo -5 800 200 200: \033[0;31mKO\033[0m" >>trace
+fi
+
+./philo 5 -800 200 200 >output 2>&1
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
+then
+    printf " \033[0;32mOK\033[0m"
+else
+    printf " \033[0;31mKO\033[0m"
+    echo "./philo 5 -800 200 200: \033[0;31mKO\033[0m" >>trace
+fi
+
+./philo 5 800 -200 200 >output 2>&1
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
+then
+    printf " \033[0;32mOK\033[0m"
+else
+    printf " \033[0;31mKO\033[0m"
+    echo "./philo 5 800 -200 200: \033[0;31mKO\033[0m" >>trace
+fi
+
+./philo 5 800 200 -200 >output 2>&1
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
+then
+    printf " \033[0;32mOK\033[0m"
+else
+    printf " \033[0;31mKO\033[0m"
+    echo "./philo 5 800 200 -200: \033[0;31mKO\033[0m" >>trace
+fi
+
+./philo 5 800 200 200 -7 >output 2>&1
+if grep -qiE "error|invalid|arg|args|argument|arguments|usage" output
+then
+    printf " \033[0;32mOK\033[0m\n\n"
+else
+    printf " \033[0;31mKO\033[0m\n\n"
+    echo "./philo 5 800 200 200 -7: \033[0;31mKO\033[0m" >>trace
+fi
 
 
-./philo 1 800 200 200 >tmp
-echo "Test 1 800 200 200 : The philosopher should not eat and should die at 800ms.";echo
-time_to_die=$(grep died <tmp | awk '{print $1}')
-if grep died <tmp >/dev/null
+# Mandatory tests
+./philo 1 800 200 200 >output
+time_to_die=$(grep -iE "died|dead" <output | awk '{print $1}')
+number_of_action=$(wc -l <output)
+echo -n "[1 800 200 200]: "
+if [ $number_of_action -eq 2 ] && grep -qiE "died|dead" <output
 then
     printf "\033[0;32mOK\033[0m"
     if [ $time_to_die -eq 800 ] || [ $time_to_die -eq 801 ]
     then
-        printf " \033[0;32mOK\033[0m\n"
+        printf " \033[0;32mOK\033[0m\n\n"
     else
-        printf " \033[0;31mKO\033[0m\n"
-        echo "Test 1 800 200 200: The philo death is not equal to 800ms (or 801ms)" >traces
+        printf " \033[0;31mKO\033[0m\n\n"
+        echo "./philo 1 800 200 200: \033[0;31mKO\033[0m : the philo should die at around 800ms" >>trace
     fi
 else
     printf "\033[0;31mKO\033[0m\n"
+    echo "./philo 1 800 200 200: \033[0;31mKO\033[0m : the philo should take his fork, wait his time_to_die and die" >>trace
 fi
 
-
-echo; echo "Test 5 800 200 200 : No philosopher should die. (10s test)";echo
-if timeout 10 ./philo 5 800 200 200 | grep died >/dev/null
+echo -n "[5 800 200 200]: "
+if timeout 10 ./philo 5 800 200 200 | grep -qiE "died|dead"
 then
-    printf "\033[0;31mKO\033[0m\n"
+    printf "\033[0;31mKO\033[0m\n\n"
+    echo "./philo 5 800 200 200: \033[0;31mKO\033[0m : no one should die" >>trace
 else
-    printf "\033[0;32mOK\033[0m\n"
+    printf "\033[0;32mOK\033[0m\n\n"
 fi
 
-
-echo; echo "Test 5 800 200 200 7 : No philosopher should die and the simulation should stop when every philosopher has eaten at least 7 times.";echo
-./philo 5 800 200 200 7 >tmp
-meal_count=$(grep eating <tmp | wc -l)
-if grep died <tmp >/dev/null
+echo -n "[5 800 200 200 7]: "
+./philo 5 800 200 200 7 >output
+meal_count=$(grep -iE "eating|eat|ate" <output | wc -l)
+if grep -qiE "died|dead" <output
 then
     printf "\033[0;31mKO\033[0m\n"
+    echo "./philo 5 800 200 200 7: \033[0;31mKO\033[0m : no one should die" >>trace
 else
     printf "\033[0;32mOK\033[0m"
     if [ $meal_count -gt 34 ]
     then
-        printf " \033[0;32mOK\033[0m\n"
+        printf " \033[0;32mOK\033[0m\n\n"
     else
-        printf " \033[0;31mKO\033[0m\n"
-        echo "Test 5 800 200 200: The philosophers did not eat 7 times" >traces
+        printf " \033[0;31mKO\033[0m\n\n"
+        echo "./philo 5 800 200 200 7: \033[0;31mKO\033[0m : all philos must eat 7 times" >>trace
     fi
 fi
 
-
-echo; echo "Test 4 410 200 200 : No philosopher should die. (10s test)";echo
-if timeout 10 ./philo 4 410 200 200 | grep died >/dev/null
+echo -n "[4 410 200 200]: "
+if timeout 10 ./philo 4 410 200 200 | grep -qiE "died|dead"
 then
-    printf "\033[0;31mKO\033[0m\n"
+    printf "\033[0;31mKO\033[0m\n\n"
+    echo "./philo 4 410 200 200: \033[0;31mKO\033[0m : no one should die" >>trace
 else
-    printf "\033[0;32mOK\033[0m\n"
+    printf "\033[0;32mOK\033[0m\n\n"
 fi
 
-
-echo; echo "Test 3 310 200 100 : One philosopher should die at 310ms.";echo
-./philo 4 310 200 100 >tmp
-time_to_die=$(grep died <tmp | awk '{print $1}')
-if grep died <tmp >/dev/null
+./philo 4 310 200 100 >output
+time_to_die=$(grep -iE "died|dead" <output | awk '{print $1}')
+echo -n "[4 310 200 100]: "
+if grep -qiE "died|dead" <output
 then
     printf "\033[0;32mOK\033[0m"
     if [ $time_to_die -eq 310 ] || [ $time_to_die -eq 311 ]
     then
-        printf " \033[0;32mOK\033[0m\n"
+        printf " \033[0;32mOK\033[0m\n\n"
     else
-        printf " \033[0;31mKO\033[0m\n"
-        echo "Test 3 310 200 100: The philo death is not equal to 310ms" >traces
+        printf " \033[0;31mKO\033[0m\n\n"
+        echo "./philo 4 310 200 100: \033[0;31mKO\033[0m : a philo should die around 310ms" >>trace
     fi
 else
     printf "\033[0;31mKO\033[0m\n"
+    echo "./philo 4 310 200 100: \033[0;31mKO\033[0m : a philo should die" >>trace
 fi
 
-
-echo; echo "Test 2 400 200 200 : No philosopher should die. (10s test)";echo
-if timeout 10 ./philo 2 400 200 200 | grep died >/dev/null
+echo -n "[2 400 200 200]: "
+if timeout 10 ./philo 2 400 200 200 | grep -qiE "died|dead"
 then
-    printf "\033[0;31mKO\033[0m\n"
+    printf "\033[0;31mKO\033[0m\n\n"
+    echo "./philo 2 400 200 200: \033[0;31mKO\033[0m : no one should die" >>trace
 else
-    printf "\033[0;32mOK\033[0m\n"
+    printf "\033[0;32mOK\033[0m\n\n"
 fi
 
-
-echo; echo "Test 2 200 200 200 : One philosopher should die at 200ms.";echo
-./philo 2 200 200 200 >tmp
-time_to_die=$(grep died <tmp | awk '{print $1}')
-if grep died <tmp >/dev/null
+./philo 2 200 200 200 >output
+time_to_die=$(grep -iE "died|dead" <output | awk '{print $1}')
+echo -n "[2 200 200 200]: "
+if grep -qiE "died|dead" <output
 then
     printf "\033[0;32mOK\033[0m"
     if [ $time_to_die -gt 199 ] || [ $time_to_die -lt 211 ]
     then
-        printf " \033[0;32mOK\033[0m\n"
+        printf " \033[0;32mOK\033[0m\n\n"
     else
-        printf " \033[0;31mKO\033[0m\n"
-        echo "Test 2 200 200 200: The death is delayed by more than 10 ms" >traces
+        printf " \033[0;31mKO\033[0m\n\n"
+        echo "./philo 4 310 200 100: \033[0;31mKO\033[0m : a philo should die around 200ms" >>trace
     fi
 else
     printf "\033[0;31mKO\033[0m\n"
+    echo "./philo 2 200 200 200: \033[0;31mKO\033[0m : a philo should die" >>trace
 fi
-echo
-rm tmp
+
+rm -f output
+cd 42_philo_tester/
